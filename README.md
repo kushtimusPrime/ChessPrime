8790dca1ae46a5622243d0735a27800797fb58de
kushtimusPrime <kushtimusprime@gmail.com>
2023-06-11 00:28:28 -0400

    Added data initialization

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Chess AI
 Based on this link: https://towardsdatascience.com/creating-a-chess-ai-using-deep-learning-d5278ea7dcf
 
+## Installation
 Install necessary libraries with 
 
 ``` pip install -r requirements.txt ```
 
+## Data
+Got dataset from: https://www.kaggle.com/datasets/robikscube/this-week-in-chess-archive?resource=download
+``` 
+sudo apt-get install unzip
+unzip archive.zip -d data/
+ ```
---
+++ train.py
@@ -0,0 +1,78 @@
+import os
+import chess
+import chess.pgn
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+
+class ChessTrainer:
+    def __init__(self,total_game_limit = 100000):
+        self.data_path_ = "data/pgns"
+        self.pgn_file_names_ = os.listdir(self.data_path_)
+        self.total_games_ = [];
+        self.total_num_games_ = 0
+        self.total_game_limit_ = total_game_limit
+        self.chess_dict_ = {
+            'p' : [1,0,0,0,0,0,0,0,0,0,0,0],
+            'P' : [0,0,0,0,0,0,1,0,0,0,0,0],
+            'n' : [0,1,0,0,0,0,0,0,0,0,0,0],
+            'N' : [0,0,0,0,0,0,0,1,0,0,0,0],
+            'b' : [0,0,1,0,0,0,0,0,0,0,0,0],
+            'B' : [0,0,0,0,0,0,0,0,1,0,0,0],
+            'r' : [0,0,0,1,0,0,0,0,0,0,0,0],
+            'R' : [0,0,0,0,0,0,0,0,0,1,0,0],
+            'q' : [0,0,0,0,1,0,0,0,0,0,0,0],
+            'Q' : [0,0,0,0,0,0,0,0,0,0,1,0],
+            'k' : [0,0,0,0,0,1,0,0,0,0,0,0],
+            'K' : [0,0,0,0,0,0,0,0,0,0,0,1],
+            '.' : [0,0,0,0,0,0,0,0,0,0,0,0],
+        }
+        self.alpha_dict_ = {
+            'a' : [0,0,0,0,0,0,0],
+            'b' : [1,0,0,0,0,0,0],
+            'c' : [0,1,0,0,0,0,0],
+            'd' : [0,0,1,0,0,0,0],
+            'e' : [0,0,0,1,0,0,0],
+            'f' : [0,0,0,0,1,0,0],
+            'g' : [0,0,0,0,0,1,0],
+            'h' : [0,0,0,0,0,0,1],
+        }
+        self.number_dict_ = {
+            1 : [0,0,0,0,0,0,0],
+            2 : [1,0,0,0,0,0,0],
+            3 : [0,1,0,0,0,0,0],
+            4 : [0,0,1,0,0,0,0],
+            5 : [0,0,0,1,0,0,0],
+            6 : [0,0,0,0,1,0,0],
+            7 : [0,0,0,0,0,1,0],
+            8 : [0,0,0,0,0,0,1],
+        }
+    
+    def initializeData(self):
+        for pgn_file_name in self.pgn_file_names_:
+            print("Game count: " + str(self.total_num_games_))
+            pgn_data_path = self.data_path_ + "/" + pgn_file_name
+            pgn = None
+            pgn = open(pgn_data_path)
+            thru_all_games = False
+            while ((self.total_num_games_ < self.total_game_limit_) and (not thru_all_games)):
+                try:
+                    game = chess.pgn.read_game(pgn)
+                    self.total_num_games_ = self.total_num_games_ + 1
+                    if game is not None:
+                        self.total_games_.append(game)
+                    else:
+                        thru_all_games = True
+                except:
+                    print("Tough")
+        print("Game count: " + str(self.total_num_games_))
+
+
+
+def main():
+    chess_trainer = ChessTrainer(1000)
+    chess_trainer.initializeData()
+
+if __name__ == "__main__":
+    main()
