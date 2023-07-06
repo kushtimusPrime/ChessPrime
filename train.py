e36d72b7f60afb192c79399215487a6e92578da9
kushtimusPrime <kushtimusprime@gmail.com>
2023-07-06 07:50:07 +0000

    Training on more games

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ def main():
     print("Number of CPU cores:", cpu_count)
     wandb.login()
     print("Starting training")
-    total_game_limit_num = 142000
+    total_game_limit_num = 315000
     train_dataset = ChessDataset(total_game_limit = total_game_limit_num)
     print("Loaded train dataset")
     test_dataset = ChessDataset(total_game_limit=total_game_limit_num/4,is_train = False)
